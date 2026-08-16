e6c95a1b363a713fbce91077d019e25a0c7f42a5
byteUUM <xyk719@126.com>
2026-08-16 23:03:38 +0800

    string

--- string.cc
+++ string.cc
@@ -22,9 +22,73 @@ void test1(sw::redis::Redis& redis)
     cout<<redis.get("key1").value()<<endl;
     
 }
+void test2(sw::redis::Redis& redis)
+{
+    redis.flushall();
+    redis.set("key2","2",std::chrono::seconds(3),sw::redis::UpdateType::NOT_EXIST);
+    auto value = redis.get("key2");
+    if(value) cout<<"key2 exist, value: "<<*value<<endl;
+    else cout<<"key2 not exist"<<endl;
+}
+void test3(sw::redis::Redis& redis)
+{
+    redis.flushall();
+    redis.mset({std::make_pair("k","1"), std::make_pair("k2","2"), std::make_pair("k3","3")});
+    
+    auto v1 = redis.get("k");
+    if(v1) cout<<v1.value()<<endl;
+    
+    auto v2 = redis.get("k2");
+    if(v2) cout<<v2.value()<<endl;
+    
+    auto v3 = redis.get("k3");
+    if(v3) cout<<v3.value()<<endl;
+
+    unordered_map<string, string> data = {
+        {"key1", "111"},
+        {"key2", "222"},
+        {"key3", "333"}
+    };
+    redis.mset(data.begin(), data.end());
+}
+
+void test4(sw::redis::Redis& redis)
+{
+    redis.flushall();
+    redis.set("key1","abcdefghijklm");
+    auto ret = redis.getrange("key1",5,-1);
+    if(!ret.empty()) cout<<"key1 exist, value: "<<ret<<endl;
+    
+    //不支持负数偏移
+    redis.setrange("key1",2,"kkkkkk");
+    auto value = redis.get("key1");
+    if(value) cout<<"key1 exist, value: "<<value.value()<<endl;
+}
+void test5(sw::redis::Redis& redis) {
+    std::cout << "incr 和 decr" << std::endl;
+    redis.flushall();
+    redis.set("key", "100");
+
+    long long result = redis.incr("key");
+    std::cout << "result: " << result << std::endl;
+
+    auto value = redis.get("key");
+    std::cout << "value: " << value.value() << std::endl;
+
+    result = redis.decr("key");
+    std::cout << "result: " << result << std::endl;
+
+    value = redis.get("key");
+    std::cout << "value: " << value.value() << std::endl;
+}
+
 int main()
 {
     sw::redis::Redis redis("tcp://127.0.0.1:6379");
-    test1(redis);
+    // test1(redis);
+    // test2(redis);
+    // test3(redis);
+    // test4(redis);
+    test5(redis);
     return 0;
 }
